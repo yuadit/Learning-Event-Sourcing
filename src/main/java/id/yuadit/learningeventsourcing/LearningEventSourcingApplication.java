12083e31b3ae18a59f770b713f444c3b06c7798d
yuhan <yuhan.adit@gmail.com>
2024-06-11 10:27:09 +0700

    Initial Spring Boot Project

---
+++ src/main/java/id/yuadit/learningeventsourcing/LearningEventSourcingApplication.java
@@ -0,0 +1,13 @@
+package id.yuadit.learningeventsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningEventSourcingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningEventSourcingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/yuadit/learningeventsourcing/LearningEventSourcingApplicationTests.java
@@ -0,0 +1,13 @@
+package id.yuadit.learningeventsourcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningEventSourcingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
